63fc28abf9cfe5ff18ca967cd0a9ad33374ed09c
LeeSuha-98 <dltngk10@naver.com>
2021-09-04 00:37:41 +0900

    1차

---
+++ Data_and_operation/src/Datatype.java
@@ -0,0 +1,13 @@
+public class Datatype {
+	public static void main(String[] args) {
+		System.out.println(6); //Number
+		System.out.println("six"); //String
+		
+		System.out.println(6+6); //String 6
+		System.out.println("6"+"6");
+		
+		System.out.println(6*6); //36
+		//System.out.println("6"*"6"); 에러가 떠서 실행할 수 없다.
+		System.out.println("1111".length()); //4, 문자열의 길이를 알려주는 연산
+	}
+}
---
+++ selfstudy01/src/selfstudy01/HelloWorldApp.java
@@ -0,0 +1,7 @@
+package selfstudy01;
+
+public class HelloWorldApp {  //파일 명과 같아야한다.
+	public static void main(String[] args) {
+	System.out.println("Hello World!!");
+	}
+}
