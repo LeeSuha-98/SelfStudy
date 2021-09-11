b85d85c80e05d8278383c006cc68ccf6f92efe98
LeeSuha-98 <dltngk10@naver.com>
2021-09-11 19:40:04 +0900

    0911 복습2

---
+++ Programming/src/ClassApp.java
@@ -0,0 +1,12 @@
+
+public class ClassApp {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(1.6)); // 소수점 내림 = floor
+		System.out.println(Math.ceil(1.6));// 소수점 올림 = ceil
+		
+	}
+
+}
