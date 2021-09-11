ab58759fe999441fea30d93494fd13abe36850d6
LeeSuha-98 <dltngk10@naver.com>
2021-09-11 18:24:47 +0900

    0911 복습1

--- Programming/src/org/opentutorials/iot/OKJavaGoInHomeInput.java
+++ Programming/src/org/opentutorials/iot/OKJavaGoInHomeInput.java
@@ -1,11 +1,16 @@
 package org.opentutorials.iot;
 
-public class OKJavaGoInHome {
+public class OKJavaGoInHomeInput {
 	//IOT 가짜코드로 맛보기
 	
 	public static void main(String[] args) {
 		
-		String id = "JAVA APT 507";
+		//String id = JOptionPane.showInputDialog("Enter a ID"); 이 코드로 조그마한 상자가 나오고 어떠한 값을 입력할때까지 
+	    //자바는 멈춰져있으며, 입력하게 되면 ID가 적용된다.
+		String id = args[0];
+		String bright = args[1];
+				
+		//String bright = JOptionPane.showInputDialog("Enter a Bright");
 		
 		// Elevator call (엘리베이터를 미리 대기시켜 놓기)
 		Elevator myElevator = new Elevator(id);  // myElevator는 변수, Elevator는 마이 엘리베이터의 데이터 타입이다.
@@ -21,6 +26,10 @@ public class OKJavaGoInHome {
 		
 		Lighting floorLamp = new Lighting(id+" / Hall Lamp");
 		floorLamp.on();
+		
+		DimmingLights moodLamp = new DimmingLights(id+"moodLamp");
+		moodLamp.setBright(Double.parseDouble(bright)); // n%만큼 밝기가 조절된다.
+		moodLamp.on();
 		
 	}
 
